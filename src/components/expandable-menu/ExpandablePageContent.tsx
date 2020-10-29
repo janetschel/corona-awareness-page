7cffa1741ebc39a75b149b1ad5510e951d4a4562
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-10-29 15:09:55 +0100

    Changed content a little bit to be more polite (lol)

--- src/components/expandable-menu/ExpandablePageContent.tsx
+++ src/components/expandable-menu/ExpandablePageContent.tsx
@@ -11,11 +11,11 @@ function ExpandablePageContent() {
       <div className="Content" id="Content">
         <Typography className="Header">Sensibilisierung mit der Corona-Pandemie im 21. Jahrhundert</Typography>
         <Typography>
-          In Deutschland gilt die Meinungsfreiheit. Dieses Gut gilt es so gut wie möglich - durch die Politik, als auch durch das Volk
-          - zu schützen, und deshalb auch in unserem Grundgesetz im fünften Artikel verankert.
+          In Deutschland gilt die Meinungsfreiheit. Dieses Gut muss so gut wie möglich - durch die Politik, als auch durch das Volk
+          - geschützt werden, und ist deshalb auch in unserem Grundgesetz im fünften Artikel verankert.
         </Typography>
         <Typography className="NoMargin">
-          Jeder darf seine Meinung frei äußern und in einen Diskurs mit der Politik treten.
+          Jeder darf seine Meinung frei äußern und in einem Diskurs mit der Politik treten.
         </Typography>
         <Typography>
           Der gesunde - und in einer Demokratie (welche wir in Deutschland übrigens haben) gewünschte - Diskurs ist allerdings dort als
@@ -23,7 +23,7 @@ function ExpandablePageContent() {
           10.000 Menschen ihr Leben gelassen.
         </Typography>
         <Typography>
-          So hoch und grauensam diese Zahl auch scheinen mögen, können wir uns glücklich schätzen, dass COVID-19 in Deutschland nur so
+          So hoch und grausam diese Zahl auch scheinen mag, können wir uns glücklich schätzen, dass COVID-19 in Deutschland nur so
           wenige Menschenleben gefordert hat.. so makaber das auch klingen mag.
         </Typography>
         <Typography className="NoMargin">
@@ -44,10 +44,10 @@ function ExpandablePageContent() {
         <Typography>
           Natürlich sind die Maßnahmen nicht perfekt, natürlich könnte man darüber nachdenken Schulen zu schließen und die Wirtschaft
           besser zu stärken, aber die <strong>kollektive Gesundheit</strong> unserer Mitbürger und Mitbürgerinnen hat auf jeden Fall
-          Priorität über eine abgebrochene Zake an deinem Krönchen, weil du mal wieder eine Maske anziehen musst.
+          Priorität über eine abgebrochene Zake an Deinem Krönchen, weil Du mal wieder eine Maske anziehen musst.
         </Typography>
         <Typography>
-          Auch wenn es vielleicht stimmen mag, dass Corona dich nie so hart erwischt, dass du dich am nächsten Tag auf einmal auf der
+          Auch wenn es vielleicht stimmen mag, dass Corona Dich nie so hart erwischt, dass du Dich am nächsten Tag auf einmal auf der
           Intensivstation wiederfindest, sollte man trotzdem auf die anderen Mitmenschen Rücksicht nehmen, für welche so eine Aussage
           vielleicht nicht selbstverständlich sein mag.
         </Typography>
@@ -67,11 +67,11 @@ function ExpandablePageContent() {
         <Typography>
           Das einzige Problem bei der Berichtserstattung, welche wir in Deutschland haben, sind die Verschwörungstheoretiker
           wie <del>Atilla Hildmann</del> oder seit neustem auch <del>Michael Wendler</del>,
-          welche <strong>bewusst</strong> Falschinformationen ohne jegliche Reue zu verpühren, blindlings an ihre Anhänger
+          welche <strong>bewusst</strong> Falschinformationen ohne jegliche Reue zu verspühren, blindlings an ihre Anhänger
           propagieren.
         </Typography>
         <Typography>
-          <strong>DAS</strong> ist das eigentliche Problem, welchen wir in Mitten der Corona-Pandemie noch zusätzlich gegenüber stehen.
+          <strong>DAS</strong> ist das eigentliche Problem, welches wir in Mitten der Corona-Pandemie noch zusätzlich gegenüber stehen.
           Schafe, welche ihrem Hüter vertrauen, und Hüter, welche das ausnutzen. Volkshetze gegen den Staat auf bester Ebene.
         </Typography>
         <Typography className="NoMargin">
@@ -79,19 +79,19 @@ function ExpandablePageContent() {
         </Typography>
         <Typography>
           Das zu realisieren ist für manche Personen (fast schon Personengruppen) sehr schwer. Kein Google, und auch keine andere
-          Suchmaschine versucht euch zu manipulieren. Also bitte <strong>nutzt diese</strong> auch und vertraut dem wissenschaftlichen
+          Suchmaschine versucht Euch zu manipulieren. Also bitte: <strong>nutzt diese</strong> auch und vertraut dem wissenschaftlichen
           Konsens.
         </Typography>
         <Typography>
           Solange wir Corona nicht sicher gegenüber stehen können, und auch noch keinen Impfstoff haben, geht die Gesundheit
-          <strong> aller Mitbürger</strong> vor unsere persönliche Entfaltung.
+          <strong> aller Mitbürger</strong> vor unserer persönlicher Entfaltung.
         </Typography>
         <Typography>
           Bleibt daheim, schaut ein bisschen Fernsehen und genießt die Zeit.. sie geht auch irgendwann wieder vorbei, und wir kehren
           in ein "normales" Leben zurück - falls wir bis dahin <strong>noch leben</strong>.
         </Typography>
         <Typography>
-          Informiert euch! Glaubt keinen Idioten, die irgendwas herunterschreien und Anhänger manipulieren wollen. Den etablierten
+          Informiert Euch! Glaubt keinen Idioten, die irgendwas herunterschreien und Anhänger manipulieren wollen. Den etablierten
           Medien in Deutschland ist <strong>zu vertrauen!</strong>
         </Typography>
         <Typography>
